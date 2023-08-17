1a08e4859126a550d13394c5c38d32fb58535163
Adriel Rasjido <adriel.rasjido@alumnos.frgp.utn.edu.ar>
2023-08-17 18:30:55 -0300

    Initial commit

---
+++ app/src/main/java/com/example/tp2_grupo_7/ActivityFormList.java
@@ -0,0 +1,14 @@
+package com.example.tp2_grupo_7;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityFormList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_list);
+    }
+}
