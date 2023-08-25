5c9a489f6ea8685bb2742cad055813cc297e0571
Adriel Rasjido <adriel.rasjido@alumnos.frgp.utn.edu.ar>
2023-08-25 02:08:41 -0300

    BASE - AGREGAR - LISTAR

--- app/src/main/java/com/example/tp2_grupo_7/ActivityFormList.java
+++ app/src/main/java/com/example/tp2_grupo_7/ActivityFormList.java
@@ -7,9 +7,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.security.PrivilegedAction;
+import java.util.ArrayList;
 
 public class ActivityFormList extends AppCompatActivity {
 
+    private ListView lv1;
+    private TextView tvdatos;
+    private String contactosItems [];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +33,47 @@ public class ActivityFormList extends AppCompatActivity {
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        lv1 = (ListView)findViewById(R.id.lv_contactos);
+        tvdatos = (TextView)findViewById(R.id.tvDatos);
+
+        ArrayList <String> lineasContactos = new ArrayList<>();
+        String contactos [] = fileList();
+        if(ExistenContactos(contactos,"contactos.txt")){
+            try {
+                InputStreamReader contacto = new InputStreamReader(openFileInput("contactos.txt"));
+                BufferedReader br = new BufferedReader(contacto);
+                //Lee la primera linea para saber si existe algun contenido
+                String linea = br.readLine();
+                while (linea != null){
+                    lineasContactos.add(linea);
+                    linea = br.readLine();
+                }
+                br.close();
+                contacto.close();
+            }catch (IOException e){
+
+            }
+
+            contactosItems = lineasContactos.toArray(new String[0]);
+            ArrayAdapter <String> adapter = new ArrayAdapter<String>(this,R.layout.list_item_contacto);
+            lv1.setAdapter(adapter);
+
+            //AL SELECCIONAR UN ITEM
+            lv1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                    tvdatos.setText("Datos: " + lv1.getItemIdAtPosition(i));
+                }
+            });
+        }
+    }
+
+    private boolean ExistenContactos(String contactos[],String NombreArchivo){
+        for(int i = 0; i < contactos.length; i++)
+            if(NombreArchivo.equals(contactos[i]))
+                return true;
+        return false;
     }
 
     @Override public boolean onCreateOptionsMenu(Menu mimenu){
